1a1e7123340e5f5a3e19d13f26f1cef103cc9644
Anshu Kumar <anshu.ac.dv@gmail.com>
2025-07-04 11:48:59 +0530

    Initial Commit

--- lib/repository/screens/splash/splash_screen.dart
+++ lib/repository/screens/splash/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:blinkit_flutter_project/domain/constants/app_colors.dart';
+import 'package:blinkit_flutter_project/repository/screens/login/login_screen.dart';
 import 'package:blinkit_flutter_project/repository/widgets/ui_helper.dart';
 import 'package:flutter/material.dart';
 
@@ -11,13 +14,26 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginScreen()),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.scaffloldbackground,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [UiHelper.constomImage(img: "splash.png")],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [UiHelper.constomImage(img: "splash.png")],
+        ),
       ),
     );
   }
